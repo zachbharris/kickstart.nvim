6c8fe538b3375f3c21e3379e4c686d408acd223f
Zach Harris <zachbharris94@gmail.com>
2024-09-05 17:35:04 -0500

    add desc to harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
 
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():add()
-      end)
+      end, { desc = 'Harpoon: Add' })
 
       vim.keymap.set('n', '<C-e>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
